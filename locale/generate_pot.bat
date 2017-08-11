559ed59294c948ea39ab32b0d0f7960a26da00c8
nosmokingbandit <nosmokingbandit@gmail.com>
2017-08-10 21:57:10 -0400

    Update po generating script to include all core py modules

--- locale/generate_pot.bat
+++ locale/generate_pot.bat
@@ -1,1 +1,1 @@
-py -3.6 C:\Python36\Tools\i18n\pygettext.py -a -v -d watcher -o watcher.po ..\templates\*.html ..\templates\*\*.html ..\templates\library\*\*.html
+py -3.6 C:\Python36\Tools\i18n\pygettext.py -a -v -d watcher -o watcher.po ..\templates\*.html ..\templates\*\*.html ..\templates\library\*\*.html ..\core\**.py
