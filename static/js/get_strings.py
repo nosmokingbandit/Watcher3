91b394b4fa27ed3f54adf7ddfb1bc0a4cb8a9231
nosmokingbandit <nosmokingbandit@gmail.com>
2017-08-10 21:47:10 -0400

    Add python script to generate JS language object
    Run `python3 get_strings.py` to generate js file with object with strings to be translated.

---
+++ static/js/get_strings.py
@@ -0,0 +1,56 @@
+import os
+import sys
+import argparse
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('-n', help='Language name.', type=str)
+parser.add_argument('-o', help='Output file name.', type=str)
+args = parser.parse_args()
+
+if not args.n:
+    print('Usage:')
+    print('get_strings.py -n language_name [-o output_file.js]')
+    sys.exit(1)
+name = args.n
+
+if args.o:
+    output = args.o
+else:
+    output = 'js_strings_{}.js'.format(name)
+
+print('Writing to output file: {}'.format(output))
+
+scripts = []
+strings = {}
+
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        if root.endswith('lib'):
+            continue
+        if file.endswith('.js'):
+            scripts.append(os.path.join(root, file))
+
+for script in scripts:
+    with open(script, 'r') as f:
+        t = f.read()
+        t = t.split('_("')[1:]
+        if len(t) == 0:
+            continue
+        for i in t:
+            string = i.split('")')[0]
+            if string not in strings.keys():
+                strings[string] = [os.path.basename(script)]
+            else:
+                strings[string].append(os.path.basename(script))
+
+print(strings)
+
+with open(output, 'w+') as f:
+    f.write(('"{}" : {{\n').format(name))
+    for string, files in strings.items():
+        ln = '\t\t"{}": "", \t//{}\n'.format(string, ', '.join(files))
+        f.write(ln)
+
+    f.write('\t\t}')
