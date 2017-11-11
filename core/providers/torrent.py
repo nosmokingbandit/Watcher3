db8bcb6cf794190e178c5bd829b427c2aea6ebbc
Unknown <nosmokingbandit@gmail.com>
2017-11-10 23:58:52 -0500

    Add TorrentDownloads torrent provider
    http://www.torrentdownloads.me/

--- core/providers/torrent.py
+++ core/providers/torrent.py
@@ -119,6 +119,11 @@ class Torrent(NewzNabProvider):
             for i in tpb_results:
                 if i not in results:
                     results.append(i)
+        if torrent_indexers['torrentdownloads']:
+            tordls = TorrentDownloads.search(imdbid, term)
+            for i in tordls:
+                if i not in results:
+                    results.append(i)
         if torrent_indexers['yts']:
             yts_results = YTS.search(imdbid, term)
             for i in yts_results:
@@ -160,6 +165,11 @@ class Torrent(NewzNabProvider):
         if torrent_indexers['thepiratebay']:
             thepiratebay_results = ThePirateBay.get_rss()
             for i in thepiratebay_results:
+                if i not in results:
+                    results.append(i)
+        if torrent_indexers['torrentdownloads']:
+            tordls = TorrentDownloads.get_rss()
+            for i in tordls:
                 if i not in results:
                     results.append(i)
         if torrent_indexers['torrentz2']:
@@ -928,4 +938,93 @@ class Zooqle(object):
                 continue
 
         logging.info('Found {} results from Zooqle.'.format(len(results)))
+        return results
+
+
+class TorrentDownloads(object):
+
+    @staticmethod
+    def search(imdbid, term):
+        proxy_enabled = core.CONFIG['Server']['Proxy']['enabled']
+
+        logging.info('Performing backlog search on TorrentDownloads for {}.'.format(imdbid))
+
+        url = 'http://www.torrentdownloads.me/rss.xml?type=search&search={}'.format(term)
+
+        try:
+            if proxy_enabled and proxy.whitelist('http://www.torrentdownloads.me') is True:
+                response = Url.open(url, proxy_bypass=True).text
+            else:
+                response = Url.open(url).text
+
+            if response:
+                return TorrentDownloads.parse(response, imdbid)
+            else:
+                return []
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except Exception as e:
+            logging.error('TorrentDownloads search failed.', exc_info=True)
+            return []
+
+    @staticmethod
+    def get_rss():
+        proxy_enabled = core.CONFIG['Server']['Proxy']['enabled']
+
+        logging.info('Fetching latest RSS from TorrentDownloads.')
+
+        url = 'http://www.torrentdownloads.me/rss2/last/4'
+
+        try:
+            if proxy_enabled and proxy.whitelist('http://www.torrentdownloads.me') is True:
+                response = Url.open(url, proxy_bypass=True).text
+            else:
+                response = Url.open(url).text
+
+            if response:
+                return TorrentDownloads.parse(response, None)
+            else:
+                return []
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except Exception as e:
+            logging.error('TorrentDownloads RSS fetch failed.', exc_info=True)
+            return []
+
+    @staticmethod
+    def parse(xml, imdbid):
+        logging.info('Parsing TorrentDownloads results.')
+
+        try:
+            items = gdata.data(fromstring(xml))['rss']['channel']['item']
+        except Exception as e:
+            logging.error('Unexpected XML format from TorrentDownloads.', exc_info=True)
+            return []
+
+        results = []
+        for i in items:
+            result = {}
+            try:
+                result['score'] = 0
+                result['size'] = i['size']['$t']
+                result['status'] = 'Available'
+                result['pubdate'] = None
+                result['title'] = i['title']['$t']
+                result['imdbid'] = imdbid
+                result['indexer'] = 'TorrentDownloads'
+                result['info_link'] = 'http://www.torrentdownloads.me{}'.format(i['link']['$t'])
+                result['torrentfile'] = magnet(i['info_hash']['$t'])
+                result['guid'] = i['info_hash']['$t']
+                result['type'] = 'magnet'
+                result['downloadid'] = None
+                result['freeleech'] = 0
+                result['download_client'] = None
+                result['seeders'] = int(i['seeders']['$t'])
+
+                results.append(result)
+            except Exception as e:
+                logging.error('Error parsing TorrentDownloads XML.', exc_info=True)
+                continue
+
+        logging.info('Found {} results from TorrentDownloads.'.format(len(results)))
         return results
