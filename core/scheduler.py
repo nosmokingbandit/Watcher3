eeee28bb1050e1fc6943e236b5ea37a97ee2b2de
Unknown <nosmokingbandit@gmail.com>
2018-01-02 23:24:49 -0500

    #175 allow speicifying min size for postprocessing scan
    User can supply minimum size for postprocessing scanner to attempt to import (default 500mb)
    
    Will help prevent attempting to process extra files (nfo, srt, 'hidden' .unix files, etc)

--- core/scheduler.py
+++ core/scheduler.py
@@ -77,6 +77,8 @@ class PostProcessingScan(object):
 
         logging.info('Scanning {} for movies to process.'.format(d))
 
+        minsize = core.CONFIG['Postprocessing']['Scanner']['minsize'] * 1048576
+
         if conf['newfilesonly']:
             t = core.scheduler_plugin.record.get('PostProcessing Scan', {}).get('last_execution')
             if not t:
@@ -86,9 +88,9 @@ class PostProcessingScan(object):
             threshold = time.mktime(le.timetuple())
 
             logging.info('Scanning for new files only (last scan: {}).'.format(d, le))
-            files = [os.path.join(d, i) for i in os.listdir(d) if os.path.getmtime(os.path.join(d, i)) > threshold]
+            files = [os.path.join(d, i) for i in os.listdir(d) if os.path.getmtime(os.path.join(d, i)) > threshold and os.path.getsize(os.path.join(d, i)) > minsize]
         else:
-            files = [os.path.join(d, i) for i in os.listdir(d)]
+            files = [os.path.join(d, i) for i in os.listdir(d) if os.path.getsize(os.path.join(d, i)) > minsize]
 
         if not files:
             logging.info('No new files found in directory scan.')
